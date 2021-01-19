988f6b994634340af5f9d0d443851be6fdd34b43
Greg Cooke <Cookie@Gregs-MacBook-Pro.local>
2021-01-19 07:49:13 +0000

    Just some EDA progress

--- Exploratory.R
+++ Exploratory.R
@@ -7,45 +7,60 @@ race_df = read_csv(paste0(wd, '/race-result-race.csv'))
 horse_df = read_csv((paste0(wd, '/race-result-horse.csv')))
 
 #### EDA
-#EW Rules
+#EW Rules - https://myracing.com/guides/guide-to-racing/many-places-horse-race/
 
 # 1-4, 1
-#5-7, 2
+# 5-7, 2 at 1/4
+# 8+ (non-handicap), 3 at 1/5
+# 12-15(handicap), 3 at 1/4
+# 16+ (handicap), 4 at 1/4
 
-
-
-
-unique(race_df$race_course)
+bet = 1
 
 full_df <- horse_df %>%
   dplyr:: left_join(subset(race_df, select = -c(sectional_time, incident_report)), 
                     by = 'race_id') %>%
-  dplyr:: mutate(declared_horse_weight = as.numeric(declared_horse_weight),
+  dplyr:: add_count(race_id,
+                    name = 'n_horses') %>%
+  dplyr:: mutate(win_odds = as.numeric(win_odds),
+                 declared_horse_weight = as.numeric(declared_horse_weight),
                  length_behind_winner = ifelse(length_behind_winner == '-', 0, as.numeric(length_behind_winner)),
                  winner = ifelse(finishing_position ==1, 
                                  TRUE, 
-                                 FALSE))
+                                 FALSE),
+                 each_way_outcome = case_when(
+                   (finishing_position == 1) ~ 1/2*(bet * win_odds) + 1/2*(bet*1/4*win_odds),
+                   between(n_horses,5,7) & between(finishing_position,2,3) ~ bet * win_odds*1/4,
+                   (n_horses>7) & between(finishing_position,2,4) ~ bet * win_odds*1/4,
+                   (7<n_horses) & between(finishing_position,2,4) & (!str_detect(full_df$race_name[1],'HANDICAP')) ~ bet * win_odds*1/5,
+                   between(n_horses,12,15) & between(finishing_position,2,4) & (str_detect(full_df$race_name[1],'HANDICAP')) ~ bet * win_odds*1/4,
+                   (15<n_horses) & between(finishing_position,2,5) & (str_detect(full_df$race_name[1],'HANDICAP')) ~ bet * win_odds*1/4,
+                   TRUE~0 
+                 ),
+                 each_way_outcome = each_way_outcome - bet,
+                 win_outcome = ifelse(finishing_position ==1, bet*win_odds, -bet))
+
+  
 
 ### Find favorite
-
-bet = 1
 
 fave_df = full_df %>%
   dplyr:: mutate(win_odds = as.numeric(win_odds)) %>%
   dplyr:: group_by(race_id) %>%
   dplyr:: filter(win_odds == min(win_odds, na.rm = T)) %>%
-  dplyr:: select(finishing_position, horse_name, win_odds, race_id, race_date) %>%
-  dplyr:: arrange(race_date) %>%
+  dplyr:: select(finishing_position, horse_name, win_odds, race_id, race_date, race_number) %>%
+  dplyr:: arrange(race_date, race_number) %>%
   dplyr:: mutate(outcome = ifelse(finishing_position ==1, bet*win_odds, -bet))
 
-fave_df$cum_outcome = cumsum(fave_df$outcome)
+fave_df$cum_outcome = 100 + cumsum(fave_df$outcome)
 
-ggplot(fave_df, aes(race_date,cum_outcome)) + 
+ggplot(fave_df, aes(race_date, cum_outcome)) + 
   geom_line() + 
-  theme_economist() + 
-  scale_color_economist() +
   ggtitle("Returns from betting on the favourite")
 
+fave_return = (fave_df[[nrow(fave_df),length(fave_df)]]-100)/100
+
+fave_sr = #find sharpe ratio (find average monthly returns, find standard deviation of returns, divide one by the other)
 
 ### Random Forest Algorithm
 library(tidymodels)
